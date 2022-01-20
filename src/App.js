7e846e1c5323ff3d65e76eb8655d5c0835ac83c0
Connor Rayburn <connorrayburn.work@gmail.com>
2022-01-19 22:48:33 -0700

    added inputs that update state on submit along with stones calculation on submit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Problem from './components/problem'
 
-function App() {
-  
+  const initialValues = {
+    timeLimit: 2,
+    boatLimit: 2,
+    totalBoats: 20,
+    travelTime: 10,
+    boatCapacity: 6,
+    unloadTime: 2,
+    loadTime: 1,
+  };
 
-  
+function App() {
+
+  const [values, setValues] = useState(initialValues)
+
+  const inputChange = (name, value) => {
+    setValues({ ...values, [name]: value });
+  };
+
+  const formSubmit = () => {
+    const newValues = {
+      timeLimit: values.timeLimit,
+      boatLimit: values.boatLimit,
+      totalBoats: values.totalBoats,
+      travelTime: values.travelTime,
+      boatCapacity: values.boatCapacity,
+      unloadTime: values.unloadTime,
+      loadTime: values.loadTime,
+    }
+
+    const stoneCalculator = (newValues) => {
+      const { timeLimit, boatLimit, totalBoats, travelTime, boatCapacity, unloadTime, loadTime } = newValues
+
+      if (2 * travelTime >= timeLimit * 60){
+        return 0;
+      } else if(boatLimit < totalBoats){
+        return Math.min(
+          (Math.floor(((timeLimit*60)/(travelTime*2)))-1) * boatLimit * boatCapacity + (Math.floor((timeLimit * 60 % (2*travelTime))/unloadTime)* boatLimit),
+          Math.floor(((timeLimit*60) - (travelTime*2))/ unloadTime) * boatLimit,
+          Math.floor(((timeLimit*60) - travelTime)/ loadTime) * boatLimit
+        )
+      } else {
+        return Math.min(
+          (Math.ceil(((timeLimit*60)/(travelTime*2 + loadTime)))-1) * totalBoats * boatCapacity + (Math.floor((timeLimit * 60 % (2*travelTime))/unloadTime)* totalBoats),
+          Math.floor(((timeLimit*60) - (travelTime*2))/ unloadTime) * totalBoats,
+          Math.floor(((timeLimit*60) - travelTime)/ loadTime) * totalBoats 
+        )       
+      }
+    }
+    console.log(stoneCalculator(newValues))
+};
 
   return (
     <React.Fragment>
@@ -13,7 +59,11 @@ function App() {
         PYRAMIDS<span>R</span>US
       </nav>
       <main className='container'>
-        <Problem />
+        <Problem
+          values={values}
+          change={inputChange}
+          submit={formSubmit}
+        />
       </main>
     </React.Fragment>
   );
--- src/components/problem.jsx
+++ src/components/problem.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 
-const Problem = () => {
+const Problem = ({ values, change, submit }) => {
+
+    const onChange = (evt) => {
+        const { name, value} = evt.target;
+        change(name, value);
+    }
+
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        submit();
+    }    
+
     return (  
-        <form>
+        <form onSubmit={onSubmit} onChange={onChange}>
                 The pharaoh of Egypt had ordered a new Pyramid to be built in the Capital, near the Nile
             river. The Pyramid would consist only of stone blocks. The mining site where the blocks are
             produced is 10 miles down the river. The only way to get to the mining site is by using boats on
-            the river. Only <span><input type='number' placeholder='2'></input></span> boats can be in the river, regardless of the direction they travel. The distance
-            in time between the mining site and the Pyramid building location in the capital is <span><input type='number' placeholder='10'></input></span> min. Each
-            boat can carry up to <span><input type='number' placeholder='6'></input></span> blocks. It takes <span><input type='number' placeholder='1'></input></span> min to load a single block on a boat and it takes <span><input type='number' placeholder='2'></input></span> min
-            to unload a block. All <span><input type='number' placeholder='2'></input></span> boats are initially located at the capital site by the river.
+            the river. Only 
+            <span><input name='boatLimit' value={values.boatLimit} onChange={onChange} onSubmit={onSubmit} type='number' placeholder='2' min='0'></input></span> boats can be in the river, regardless of the direction they travel. The distance
+            in time between the mining site and the Pyramid building location in the capital is 
+            <span><input name='travelTime' value={values.travelTime} onChange={onChange} onSubmit={onSubmit} type='number' placeholder='10' min='0'></input></span> min. Each
+            boat can carry up to 
+            <span><input name='boatCapacity' value={values.boatCapacity} onChange={onChange} onSubmit={onSubmit} type='number' placeholder='6' min='0'></input></span> blocks. It takes 
+            <span><input name='loadTime' value={values.loadTime} onChange={onChange} onSubmit={onSubmit} type='number' placeholder='1' min='0'></input></span> min to load a single block on a boat and it takes 
+            <span><input name='unloadTime' value={values.unloadTime} onChange={onChange} onSubmit={onSubmit} type='number' placeholder='2' min='0'></input></span> min
+            to unload a block. All 
+            <span><input name='totalBoats' value={values.totalBoats} onChange={onChange} onSubmit={onSubmit} type='number' placeholder='20' min='0'></input></span> boats are initially located at the capital site by the river. How many stones can be unloaded at the camp site in 
+            <span><input name='timeLimit' value={values.timeLimit} onChange={onChange} onSubmit={onSubmit} type='number' placeholder='2' min='0'></input></span> hours?<br/>
+            <button className='btn btn-danger'>Calculate</button>
         </form>
     );
 }
